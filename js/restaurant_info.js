899ac47c411405d7592c85394ad7e0d0242f1fd8
ibrahim.1996 <ibrahim.1996@outlook.com>
2018-05-15 12:59:47 +0200

    Small fix

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -98,7 +98,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
-  title.classList = 'sr-only';
+  title.setAttribute('hidden', '');
   container.appendChild(title);
 
   if (!reviews) {
